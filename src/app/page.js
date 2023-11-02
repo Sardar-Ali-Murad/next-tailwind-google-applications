2a61daed2a06e50435af88e41d2e338f32ff2dfc
Sardar-Ali-Murad <mar389786@gmail.com>
2023-11-02 16:16:45 +0500

    removing the build error

--- src/app/page.js
+++ src/app/page.js
@@ -205,7 +205,6 @@ const page = () => {
           <textarea
             className="block min-h-[300px] p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Your Translation Will Appear Here..."
-            value={translatedText}
           >
             {translatedText}
           </textarea>
